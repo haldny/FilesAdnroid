7fabf8a8552409357b503ca0d87afd5157ba5aa3
Haldny <haldny@motorola.com>
2020-10-20 21:45:32 -0300

    Jetpack security

--- app/src/main/java/com/haldny/filesandroid/MainActivity.kt
+++ app/src/main/java/com/haldny/filesandroid/MainActivity.kt
@@ -5,37 +5,58 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.security.crypto.EncryptedFile
+import androidx.security.crypto.MasterKey
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     companion object {
         const val FILE_NAME = "myfilename.txt"
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var secretFile: File
+    private lateinit var encryptedFile: EncryptedFile
 
+    private val fileEncrypted: File by lazy {
+        File(filesDir, FILE_NAME)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initUIElements()
+        init()
     }
 
-    override fun onClick(view: View?) {
-        when (view?.id) {
-            R.id.btnWrite -> writeOnInternalDirectory()
-            R.id.btnRead -> readFromInternalDirectory()
-        }
+    private fun init() {
+        secretFile = File(filesDir, FILE_NAME)
+
+        encryptedFile = EncryptedFile.Builder(
+            this,
+            secretFile,
+            MasterKey.Builder(this).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build(),
+            EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+        ).build()
     }
 
-    private fun readFromInternalDirectory() {
+    fun readFromInternalDirectory(view: View) {
         this.openFileInput(FILE_NAME).use {
             tvContent.text = it.readBytes().decodeToString()
         }
     }
 
-    private fun writeOnInternalDirectory() {
+    fun readFromInternalDirectoryEncrypted(view: View) {
+        encryptedFile.openFileInput().use {
+            tvContent.text = it.readBytes().decodeToString()
+        }
+    }
+
+    fun writeOnInternalDirectory(view: View) {
         val myFileContent = "Name: ${etName.text} - Age: ${etAge.text}"
 
         this.openFileOutput(FILE_NAME, Context.MODE_PRIVATE).use {
@@ -45,8 +66,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         Toast.makeText(this, "Saved content: $myFileContent", Toast.LENGTH_LONG).show()
     }
 
-    private fun initUIElements() {
-        btnWrite.setOnClickListener(this)
-        btnRead.setOnClickListener(this)
+    fun writeOnInternalDirectoryEncrypted(view: View) {
+        val myFileContent = "Name: $${etName.text} - Age: ${etAge.text}"
+
+        secretFile.delete()
+
+        encryptedFile.openFileOutput().use {
+            it.write(myFileContent.toByteArray())
+        }
+
+        Toast.makeText(this, "Saved encrypted content: $myFileContent", Toast.LENGTH_LONG).show()
     }
+
+    private fun printFileContent(file: File) {
+        if (file.exists()) {
+            val strBuilder = StringBuilder()
+            BufferedReader(FileReader(file)).readLines().forEach {
+                strBuilder.append(it)
+            }
+            tvContent.text = strBuilder.toString()
+        } else
+            Toast.makeText(this, "File not found!", Toast.LENGTH_SHORT).show()
+    }
+
 }
